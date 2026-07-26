5101d7ffea904dccc66d952bac2fdf3c7a776d16
Jen Khoo <jenkhoo040610@gmail.com>
2026-07-26 22:08:50 +0800

    Add files via upload

---
+++ Demo/Migrations/20260726135839_MenuAndOrderingDB.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Menu_and_Ordering_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class MenuAndOrderingDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ProfilePhoto = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    FailedLoginCount = table.Column<int>(type: "int", nullable: false),
+                    LockoutUntil = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    UnitPrice = table.Column<decimal>(type: "decimal(6,2)", nullable: false),
+                    IsAvailable = table.Column<bool>(type: "bit", nullable: false),
+                    Stock = table.Column<int>(type: "int", nullable: false),
+                    CategoryId = table.Column<string>(type: "nvarchar(5)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Carts",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(5)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Carts_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    OrderDateTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    TotalAmount = table.Column<decimal>(type: "decimal(6,2)", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(5)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserTokens",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    Token = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    TokenType = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    IsUsed = table.Column<bool>(type: "bit", nullable: false),
+                    Expire = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(5)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserTokens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserTokens_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductPhotos",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    PhotoUrl = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    ProductId = table.Column<string>(type: "nvarchar(5)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductPhotos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductPhotos_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CartItems",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    CartId = table.Column<string>(type: "nvarchar(5)", nullable: false),
+                    ProductId = table.Column<string>(type: "nvarchar(5)", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CartItems_Carts_CartId",
+                        column: x => x.CartId,
+                        principalTable: "Carts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CartItems_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    OrderId = table.Column<string>(type: "nvarchar(5)", nullable: false),
+                    ProductId = table.Column<string>(type: "nvarchar(5)", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    UnitPrice = table.Column<decimal>(type: "decimal(6,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    PaymentMethod = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(6,2)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    PaidDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    OrderId = table.Column<string>(type: "nvarchar(5)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Payments_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_CartId",
+                table: "CartItems",
+                column: "CartId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_ProductId",
+                table: "CartItems",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carts_UserId",
+                table: "Carts",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_OrderId",
+                table: "OrderDetails",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_ProductId",
+                table: "OrderDetails",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_UserId",
+                table: "Orders",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_OrderId",
+                table: "Payments",
+                column: "OrderId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductPhotos_ProductId",
+                table: "ProductPhotos",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserTokens_UserId",
+                table: "UserTokens",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CartItems");
+
+            migrationBuilder.DropTable(
+                name: "OrderDetails");
+
+            migrationBuilder.DropTable(
+                name: "Payments");
+
+            migrationBuilder.DropTable(
+                name: "ProductPhotos");
+
+            migrationBuilder.DropTable(
+                name: "UserTokens");
+
+            migrationBuilder.DropTable(
+                name: "Carts");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
---
+++ Demo/Models/DB.cs
@@ -0,0 +1,216 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Demo.Models;
+
+public class DB(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserToken> UserTokens { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<ProductPhoto> ProductPhotos { get; set; }
+    public DbSet<Cart> Carts { get; set; }
+    public DbSet<CartItem> CartItems { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
+    public DbSet<Payment> Payments { get; set; }
+}
+
+public class User
+{
+    [Key, MaxLength(5)]
+    public string Id { get; set; }
+
+    [Required, MaxLength(100)]
+    public string Name { get; set; }
+
+    [Required, MaxLength(10)]
+    public string Role { get; set; }
+
+    [Required, MaxLength(255)]
+    public string Password { get; set; }
+
+    [Required, MaxLength(100)]
+    public string Email { get; set; }
+
+    [MaxLength(255)]
+    public string ProfilePhoto { get; set; }
+
+    public int FailedLoginCount { get; set; } = 0;
+
+    public DateTime? LockoutUntil { get; set; }
+
+    public List<Order> Orders { get; set; } = [];
+
+    public Cart? Cart { get; set; }
+
+    public List<UserToken> UserTokens { get; set; } = [];
+}
+
+public class UserToken
+{
+    [Key, MaxLength(5)]
+    public string Id { get; set; }
+
+    [Required, MaxLength(255)]
+    public string Token { get; set; }
+
+    [Required, MaxLength(10)]
+    public string TokenType { get; set; }
+
+    public bool IsUsed { get; set; }
+    public DateTime? Expire { get; set; }
+
+    [Required]
+    public string UserId { get; set; }
+
+    public User User { get; set; }
+}
+
+public class Category
+{
+    [Key, MaxLength(5)]
+    public string Id { get; set; }
+
+    [Required, MaxLength(100)]
+    public string Name { get; set; }
+
+    public List<Product> Products { get; set; } = [];
+}
+
+public class Product
+{
+    [Key, MaxLength(5)]
+    public string Id { get; set; }
+
+    [Required, MaxLength(100)]
+    public string Name { get; set; }
+
+    [Column(TypeName = "decimal(6,2)")]
+    public decimal UnitPrice { get; set; }
+
+    public bool IsAvailable { get; set; }
+
+    public int Stock { get; set; }
+
+    [Required]
+    public string CategoryId { get; set; }
+    public Category Category { get; set; }
+
+    public List<ProductPhoto> Photos { get; set; } = [];
+}
+
+public class ProductPhoto
+{
+    [Key, MaxLength(5)]
+    public string Id { get; set; }
+
+    [MaxLength(255)]
+    public string PhotoUrl { get; set; }
+
+    [Required]
+    public string ProductId { get; set; }
+
+    public Product Product { get; set; }
+
+}
+
+public class Cart
+{
+    [Key, MaxLength(5)]
+    public string Id { get; set; }
+
+    [Required]
+    public string UserId { get; set; }
+
+    public User User { get; set; }
+
+    public List<CartItem> CartItems { get; set; } = [];
+}
+
+public class CartItem
+{
+    [Key, MaxLength(5)]
+    public string Id { get; set; }
+
+    [Required]
+    public string CartId { get; set; }
+
+    [Required]
+    public string ProductId { get; set; }
+
+    public int Quantity { get; set; }
+
+    public Cart Cart { get; set; }
+
+    public Product Product { get; set; }
+}
+
+public class Order
+{
+    [Key, MaxLength(5)]
+    public string Id { get; set; }
+
+    public DateTime? OrderDateTime { get; set; }
+
+    [MaxLength(10)]
+    public string Status { get; set; }
+
+    [Column(TypeName = "decimal(6,2)")]
+    public decimal TotalAmount { get; set; }
+
+    [Required]
+    public string UserId { get; set; }
+
+    public User User { get; set; }
+
+    public List<OrderDetail> Details { get; set; } = [];
+
+    public Payment? Payment { get; set; }
+}
+
+public class OrderDetail
+{
+    [Key, MaxLength(5)]
+    public string Id { get; set; }
+
+    [Required]
+    public string OrderId { get; set; }
+
+    [Required]
+    public string ProductId { get; set; }
+
+    public int Quantity { get; set; }
+
+    [Column(TypeName = "decimal(6,2)")]
+    public decimal UnitPrice { get; set; }
+
+    public Order Order { get; set; }
+
+    public Product Product { get; set; }
+}
+
+public class Payment
+{
+    [Key, MaxLength(5)]
+    public string Id { get; set; }
+
+    [MaxLength(100)]
+    public string PaymentMethod { get; set; }
+
+    [Column(TypeName = "decimal(6,2)")]
+    public decimal Amount { get; set; }
+
+    [MaxLength(10)]
+    public string Status { get; set; }
+
+    public DateTime? PaidDate { get; set; }
+
+    [Required]
+    public string OrderId { get; set; }
+
+    public Order Order { get; set; }
+}
+
