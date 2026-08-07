9ac3f7af0d0b475bb9a12af5ae819fb203589db7
Jen Khoo <jenkhoo040610@gmail.com>
2026-08-08 00:30:12 +0800

    add cart controller

---
+++ Demo/Controllers/CartController.cs
@@ -0,0 +1,6 @@
+﻿namespace Demo.Controllers
+{
+    public class CartController
+    {
+    }
+}
--- Demo/obj/Debug/net10.0/Demo.AssemblyInfo.cs
+++ Demo/obj/Debug/net10.0/Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a0b929d465568aea7ea2d682276c9d606f5a8d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7a90bb83be813c1fdfec3e5995177b27ba548fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
