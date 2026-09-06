d4e284c9df3d4a3a8bb533598b95d66bf79c3ffe
Aaron <aaronlimjiarou@gmail.com>
2026-09-06 13:44:30 +0800

    Member registration feature

--- Demo/Models/UserViewModel.cs
+++ Demo/Models/UserViewModel.cs
@@ -20,24 +20,34 @@ public class LoginVM
 
 public class RegisterVM
 {
+    [Required(ErrorMessage = "Email is required.")]
     [StringLength(100)]
-    [EmailAddress]
+    [RegularExpression(@"^.+@gmail\.com$", ErrorMessage = "Only @gmail.com format is allowed.")]
     [Remote("CheckEmail", "User", ErrorMessage = "Duplicated {0}.")]
     public string Email { get; set; }
 
+    [Required(ErrorMessage = "Name is required.")]
+    [StringLength(100)]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Phone number is required.")]
+    [RegularExpression(@"01[0-9]-[0-9]{7,8}$", ErrorMessage = "Invalid format.")]
+    [DisplayName("Phone Number")]
+    public string Phone { get; set;  }
+
+    [Required(ErrorMessage = "Password is required.")]
     [StringLength(100, MinimumLength = 5)]
     [DataType(DataType.Password)]
     public string Password { get; set; }
 
+    [Required(ErrorMessage = "Confirm Password is required.")]
     [StringLength(100, MinimumLength = 5)]
-    [Compare("Password")]
+    [Compare("Password", ErrorMessage = "Both passwords do not match.")]
     [DataType(DataType.Password)]
     [DisplayName("Confirm Password")]
     public string Confirm { get; set; }
 
-    [StringLength(100)]
-    public string Name { get; set; }
-
+    [Required(ErrorMessage = "Photo is required.")]
     public IFormFile Photo { get; set; }
 }
 
