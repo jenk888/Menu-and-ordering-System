e190673eaa57ee9dbbebc6a9aa036061b3ace11e
Aaron <aaronlimjiarou@gmail.com>
2026-09-09 20:41:00 +0800

    Vouchers (member can claim voucher)

---
+++ Demo/Models/VoucherViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Demo.Models
+{
+    public class VoucherClaimVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public decimal MinimumSpend { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public int? ExpiryDurationDays { get; set; }
+        public bool AlreadyClaimed { get; set; }
+    }
+}
